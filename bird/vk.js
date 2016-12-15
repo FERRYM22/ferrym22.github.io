943b7592520ea2c19448d6467327e4132c897d26
FERRYM22 <ferrym22@yandex.ru>
2016-12-15 11:50:25 +0300

    Update vk.js

--- bird/vk.js
+++ bird/vk.js
@@ -1,10 +1,24 @@
+// get VK API	
+var getUserName = function() {
+VK.api('users.get',{'fields': 'photo_50'},function(data){
+	name = data.response[0].first_name;
+	urlPhoto = data.response[0].photo_50;
+	
+});
+};
 
-var user = {
-	name:'3',
-	getName: function()
-	{
-       VK.api('users.get',{'fields': 'photo_50,first_name'},function(data){
-       	this.name = data.response[0].first_name; });
-       return this.name;
-	}
+var getAva = function(ss) {
+	var Avatar = game.newImageObject({
+             file: ss,
+             x:5 , y:5
+             });
+	Avatar.draw();
+	brush.drawText({
+			x : 60,
+			y : 5,
+			text : name,
+			size : 20,
+			color : 'white',
+			font : 'myFont',
+		});
 };
