004cec2e5a8248cc87a24d18654738e3626175c4
FERRYM22 <ferrym22@yandex.ru>
2016-12-15 11:20:50 +0300

    Update vk.js

--- bird/vk.js
+++ bird/vk.js
@@ -3,7 +3,7 @@ var user = {
 	name:'',
 	getName: function()
 	{
-       VK.api('users.get',{'fields': 'photo_50'},function(data){
+       VK.api('users.get',{'fields': 'photo_50,first_name'},function(data){
        	name = data.response[0].first_name; });
        return this.name;
 	}
